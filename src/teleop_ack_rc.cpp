0f7228fcc33102fda148f0d6e63c7cc911bd9934
Elijah Stickel <estickel@umich.edu>
2026-03-22 05:41:40 -0400

    Initial Commit: working rc_joy_node and teleop_ack_rc node, with launch files completed.

---
+++ src/teleop_ack_rc.cpp
@@ -0,0 +1,15 @@
+#include <memory>
+#include <rclcpp/rclcpp.hpp>
+#include "teleop_ack_rc/teleop_ack_rc_node.hpp"
+
+/* main: entry point for the teleop_ack_rc node */
+int main(int argc, char * argv[]) {
+    rclcpp::init(argc, argv);
+    
+    auto node = std::make_shared<teleop_ack_rc::TeleopAckRcNode>(rclcpp::NodeOptions());
+    
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    
+    return 0;
+}
